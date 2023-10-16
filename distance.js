c772fed50160c551861ca477f733f2ac506d1398
Mikhail Makharia <makharia.study@gmail.com>
2023-10-16 11:02:56 +0300

    fixed task five!

--- distance.js
+++ distance.js
@@ -1,15 +1,10 @@
 const readlineSync = require('readline-sync');
 
+
 const distance = (x1, y1, x2, y2) => {
     var distanceX = x2 - x1;
     var distanceY = y2 - y1;
-    if(distanceX < 0){
-        distanceX = -distanceX;
-    }
-    if(distanceY < 0){
-        distanceY = -distanceY;
-    }
-    return distanceX, distanceY
+    return (distanceX^2 + distanceY^2)^0.5
 }
 
 // logging the return of distance converter function after requesting input:
